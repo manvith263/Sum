6a6e3b44fa0e6cbc870e8a37a069a2d9adc32652
Manvith Penklagar <manvith263@gmail.com>
2021-05-21 06:50:40 +0200

    Initial commit

---
+++ Addition.cpp
@@ -0,0 +1,18 @@
+//Simple addition program
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x,y, sum;
+    cout<<"enter the integer:" ;
+    cin>>x>>y;
+
+    //sum of two digit number in stored is variable sumoftwonumbers
+    sum = x + y;
+
+    //print sum
+    cout<<x<<"+"<<y<<"="<<sum;
+
+    return 0;
+}
